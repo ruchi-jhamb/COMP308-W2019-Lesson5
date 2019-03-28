da9352ac5f9e174bbcb3402382ff2b8567e92ff2
Ruchi <r144@my.centennialcollege.ca>
2019-03-28 01:07:53 -0400

    URI Commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  URI: "mongodb://localhost/test"
+  //URI: "mongodb://localhost/test"
 
-  //URI: "mongodb+srv://ruchi:12345@cluster-vyciz.azure.mongodb.net/COMP308-W2019-Lesson4"
+  URI: "mongodb + srv://ruchi:12345@cluster-vyciz.azure.mongodb.net/test"
 };
